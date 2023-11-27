5b0ad7ca9679acddceca77dc1b3d55fbf5b8c3c8
poran120 <mrporanyt120@gmail.com>
2023-11-27 21:39:15 +0600

    fully responsive login page verify page  and signup done

--- verify.js
+++ verify.js
@@ -1,23 +1,31 @@
-document.addEventListener("DOMContentLoaded", function(event) {
-    function handleOTPInput() {
-        const inputs = document.querySelectorAll('#otpNumberCounter > *[id]');
+document.addEventListener("DOMContentLoaded", function (event) {
+  function handleOTPInput() {
+    const inputs = document.querySelectorAll("#otpNumberCounter > *[id]");
 
-        inputs.forEach((input, i) => {
-            input.addEventListener('keydown', (event) => {
-                const isBackspace = event.key === 'Backspace';
-                const isLastInput = i === inputs.length - 1;
+    inputs.forEach((input, i) => {
+      console.log(i, "hello");
+      input.addEventListener("keydown", (event) => {
+        console.log(event, "hellllddl");
+        const isBackspace = event.key === "Backspace";
+        const isLastInput = i === inputs.length - 1;
 
-                if (isBackspace) {
-                    inputs[i].value = '';
-                    if (i !== 0) inputs[i - 1].focus();
-                } else if (!isLastInput && (event.keyCode >= 48 && event.keyCode <= 57 || event.keyCode >= 65 && event.keyCode <= 90)) {
-                    inputs[i].value = isLastInput ? '' : event.key || String.fromCharCode(event.keyCode);
-                    if (!isLastInput) inputs[i + 1].focus();
-                    event.preventDefault();
-                }
-            });
-        });
-    }
+        if (isBackspace) {
+          inputs[i].value = "";
+          if (i !== 0) inputs[i - 1]?.focus();
+        } else if (
+          !isLastInput &&
+          ((event.keyCode >= 48 && event.keyCode <= 57) ||
+            (event.keyCode >= 65 && event.keyCode <= 90))
+        ) {
+          inputs[i].value = isLastInput
+            ? ""
+            : event.key || String.fromCharCode(event.keyCode);
+          if (!isLastInput) inputs[i + 1].focus();
+          event.preventDefault();
+        }
+      });
+    });
+  }
 
-    handleOTPInput();
+  handleOTPInput();
 });
